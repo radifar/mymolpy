7f3a380dfe6ea57938630c4298c9c9478e078759
radifar <m.radifar05@gmail.com>
2020-06-21 08:09:13 +0700

    add Raise Error in calculate_distance

--- mymolpy/functions.py
+++ mymolpy/functions.py
@@ -63,8 +63,12 @@ Namespaces are one honking great idea -- let's do more of those!
 
 def calculate_distance(rA, rB):
     # This function calculates the distance between two points given as numpy arrays.
+    if isinstance(rA,np.ndarray) is False or isinstance(rB,np.ndarray) is False:
+        raise TypeError("rA and rB must be numpy arrays")
     d=(rA-rB)
     dist=np.linalg.norm(d)
+    if dist == 0.0:
+        raise Exception("Two atoms are located in the same point in space")
     return dist
 
 def open_pdb(f_loc):
