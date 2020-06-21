59e671c783a24ba065a67abb36faf2627bc7127b
radifar <m.radifar05@gmail.com>
2020-06-21 12:06:30 +0700

    added Docstring, fix variable name, added whitespaces to calculate_distance

--- mymolpy/functions.py
+++ mymolpy/functions.py
@@ -62,14 +62,36 @@ Namespaces are one honking great idea -- let's do more of those!
     return quote
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+    
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+
     if isinstance(rA,np.ndarray) is False or isinstance(rB,np.ndarray) is False:
         raise TypeError("rA and rB must be numpy arrays")
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    if dist == 0.0:
+
+    dist_vec = (rA - rB)
+    distance = np.linalg.norm(dist_vec)
+    
+    if distance == 0.0:
         raise Exception("Two atoms are located in the same point in space")
-    return dist
+        
+    return distance
 
 def open_pdb(f_loc):
     # This function reads in a pdb file and returns the atom names and coordinates.
@@ -87,7 +109,7 @@ def open_pdb(f_loc):
     
     coords = np.array(coordinates)
     symbols = np.array(symbols)
-    
+
     return symbols, coords
 
 atomic_weights = {
