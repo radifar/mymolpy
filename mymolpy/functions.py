8622ad1d6c093d188e1f50c674ede47bb6f80963
radifar <m.radifar05@gmail.com>
2020-06-22 09:14:11 +0700

    move open_pdb to io/pdb.py

--- mymolpy/functions.py
+++ mymolpy/functions.py
@@ -63,43 +63,6 @@ Namespaces are one honking great idea -- let's do more of those!
     return quote
 
 
-def open_pdb(file_location):
-    """Open and read coordinates and atom symbols from a pdb file.
-
-    The pdb file must specify the atom elements in the last column, and follow
-    the conventions outlined in the PDB format specification.
-
-    Parameters
-    ----------
-    file_location : str
-        The location of the pdb file to read in.
-
-    Returns
-    -------
-    coords : np.ndarray
-        The coordinates of the pdb file.
-    symbols : list
-        The atomic symbols of the pdb file.
-
-    """
-
-    with open(file_location) as f:
-        data = f.readlines()
-
-    coordinates = []
-    symbols = []
-    for line in data:
-        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
-            symbols.append(line[76:79].strip())
-            atom_coords = [float(x) for x in line[30:55].split()]
-            coordinates.append(atom_coords)
-
-    coords = np.array(coordinates)
-    symbols = np.array(symbols)
-
-    return symbols, coords
-
-
 def open_xyz(file_location):
 
     # Open an xyz file and return symbols and coordinates.
