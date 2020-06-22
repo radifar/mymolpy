7961286982e1a8bada2aa2296f337f18b192714c
radifar <m.radifar05@gmail.com>
2020-06-22 17:32:30 +0700

    testing expected exceptions in build_bond_list

--- mymolpy/molecule.py
+++ mymolpy/molecule.py
@@ -4,14 +4,18 @@ Miscelaneous function related to molecule
 
 from .measure import calculate_distance
 
+
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
 
     # Find the bonds in a molecule (set of coordinates) based on distance criteria.
+    if min_bond < 0:
+        raise ValueError("Invalid minimum bond distance entered! Minimum bond distance must be greater than zero!")
+
     bonds = {}
     num_atoms = len(coordinates)
 
     for atom1 in range(num_atoms):
-        for atom2 in range(atom1+1, num_atoms):
+        for atom2 in range(atom1 + 1, num_atoms):
             distance = calculate_distance(coordinates[atom1], coordinates[atom2])
             if distance > min_bond and distance < max_bond:
                 bonds[(atom1, atom2)] = distance
--- mymolpy/tests/test_molecule.py
+++ mymolpy/tests/test_molecule.py
@@ -3,6 +3,7 @@ Unit and regression test for the molecule module
 """
 
 import mymolpy
+import pytest
 import numpy as np
 
 def test_build_bond_list():
@@ -21,3 +22,17 @@ def test_build_bond_list():
 
     for bond_length in bonds.values():
         assert bond_length == 1.4
+
+
+def test_build_bond_failure():
+
+    coordinates = np.array([
+        [1, 1, 1],
+        [2.4, 1, 1],
+        [-0.4, 1, 1],
+        [1, 1, 2.4],
+        [1, 1, -0.4]
+    ])
+
+    with pytest.raises(ValueError):
+        bonds = mymolpy.build_bond_list(coordinates, min_bond=-1)
