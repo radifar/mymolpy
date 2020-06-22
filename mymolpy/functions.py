33861abb7dff85bb4a5f313b2c66b4b5a08293a4
radifar <m.radifar05@gmail.com>
2020-06-22 09:06:11 +0700

    move calculate_distance and calculate_angle to measure.py

--- mymolpy/functions.py
+++ mymolpy/functions.py
@@ -63,38 +63,6 @@ Namespaces are one honking great idea -- let's do more of those!
     return quote
 
 
-def calculate_distance(rA, rB):
-    """Calculate the distance between two points.
-
-    Parameters
-    ----------
-    rA, rB : np.ndarray
-        The coordinates of each point.
-
-    Returns
-    -------
-    distance : float
-        The distance between the two points.
-    
-    Examples
-    --------
-    >>> r1 = np.array([0, 0, 0])
-    >>> r2 = np.array([0, 0.1, 0])
-    >>> calculate_distance(r1, r2)
-    0.1
-    """
-
-    if isinstance(rA, np.ndarray) is False or isinstance(rB, np.ndarray) is False:
-        raise TypeError("rA and rB must be numpy arrays")
-
-    dist_vec = (rA - rB)
-    distance = np.linalg.norm(dist_vec)
-
-    if distance == 0.0:
-        raise Exception("Two atoms are located in the same point in space")
-
-    return distance
-
 def open_pdb(file_location):
     """Open and read coordinates and atom symbols from a pdb file.
 
@@ -195,19 +163,6 @@ def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi
         plt.savefig(save_location, dpi=dpi, graph_min=0, graph_max=2)
 
     return ax
-
-
-def calculate_angle(rA, rB, rC, degrees=False):
-    # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
-    # by setting degrees=True
-    AB = rB - rA
-    BC = rB - rC
-    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
-
-    if degrees:
-        return np.degrees(theta)
-    else:
-        return theta
 
 
 def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_max=2):
