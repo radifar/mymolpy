e447e950175e56a0dfc675d8f76f75e67e255336
radifar <m.radifar05@gmail.com>
2020-06-22 09:15:57 +0700

    move open_xyz and write_xyz to io/xyz.py

--- mymolpy/functions.py
+++ mymolpy/functions.py
@@ -63,29 +63,6 @@ Namespaces are one honking great idea -- let's do more of those!
     return quote
 
 
-def open_xyz(file_location):
-
-    # Open an xyz file and return symbols and coordinates.
-    xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype='unicode')
-    symbols = xyz_file[:, 0]
-    coords = (xyz_file[:, 1:])
-    coords = coords.astype(np.float)
-    return symbols, coords
-
-
-def write_xyz(file_location, symbols, coordinates):
-
-    # Write an xyz file given a file location, symbols, and coordinates.
-    num_atoms = len(symbols)
-
-    with open(file_location, 'w+') as f:
-        f.write('{}\n'.format(num_atoms))
-        f.write('XYZ file\n')
-
-        for i in range(num_atoms):
-            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], coordinates[i, 0], coordinates[i, 1], coordinates[i, 2]))
-
-
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
