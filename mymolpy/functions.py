460701337d4fe1cde95c69053a586631e39e4e90
radifar <m.radifar05@gmail.com>
2020-06-22 09:12:09 +0700

    move build_bond_list to molecule.py

--- mymolpy/functions.py
+++ mymolpy/functions.py
@@ -123,21 +123,6 @@ def write_xyz(file_location, symbols, coordinates):
             f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], coordinates[i, 0], coordinates[i, 1], coordinates[i, 2]))
 
 
-def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
-
-    # Find the bonds in a molecule (set of coordinates) based on distance criteria.
-    bonds = {}
-    num_atoms = len(coordinates)
-
-    for atom1 in range(num_atoms):
-        for atom2 in range(atom1, num_atoms):
-            distance = calculate_distance(coordinates[atom1], coordinates[atom2])
-            if distance > min_bond and distance < max_bond:
-                bonds[(atom1, atom2)] = distance
-
-    return bonds
-
-
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
